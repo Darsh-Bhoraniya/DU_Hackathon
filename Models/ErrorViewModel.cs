a0a367afc49b72443fab47f1ba49824729c5614a
kamlesh_patel <59503901+kamlesh-rkj@users.noreply.github.com>
2024-02-13 11:04:39 +0530

    deleted old project

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AccountSoftware.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
